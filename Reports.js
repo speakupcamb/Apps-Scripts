278e6b7b2bc7063955265f8b6763f7a783534005
Paul Ventresca <psventresca@gmail.com>
2018-11-12 07:40:46 -0500

    Updated the "Members Not Attended" logic as discussed

--- Reports.js
+++ Reports.js
@@ -19,12 +19,11 @@ function Reports() {
   sheets.writeDataToSheet(airtable.meetingData, sheets.meetingSheet);
   sheets.writeDataToSheet(airtable.activityData, sheets.activitySheet);
   sheets.writeDataToSheet(airtable.duesData, sheets.duesSheet);
-  sheets.findAbsenteeMembers(sheets.personSheet, sheets.notAttendedSheet);
+  sheets.writeDataToSheet(airtable.notAttendedData, sheets.notAttendedSheet);
   
   //Closing ceremonies
   sheets.analyticsSheet.getRange(2, 1).setValue("Last Refresh: " + getTimestamp());
   
-  //*** Ray, is there a better way to do this in a loop? ***
   //Resize columns and return cursor to first cell for all sheets
   sheets.personSheet.sort(1, true);
   sheets.personSheet.autoResizeColumns(1, sheets.personSheet.getLastColumn());
@@ -32,10 +31,10 @@ function Reports() {
   sheets.meetingSheet.sort(1, true);
   sheets.meetingSheet.autoResizeColumns(1, sheets.meetingSheet.getLastColumn());
   sheets.meetingSheet.getRange(2, 1).activate();
-  sheets.meetingSheet.sort(1, false);
+  sheets.activitySheet.sort(5, true);
   sheets.activitySheet.autoResizeColumns(1, sheets.activitySheet.getLastColumn());
   sheets.activitySheet.getRange(2, 1).activate();
-  sheets.activitySheet.sort(5, true);
+  sheets.notAttendedSheet.sort(2, true);
   sheets.notAttendedSheet.autoResizeColumns(1, sheets.notAttendedSheet.getLastColumn());
   sheets.notAttendedSheet.getRange(2, 1).activate();
   sheets.duesSheet.sort(1, false);
